223df46bdb2b847ff1a0403146e098140372d238
mede <medegm@mail.ru>
2023-08-30 14:32:52 +0800

    fixed losing and winning brain-even

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -5,6 +5,7 @@ import readlineSync from 'readline-sync';
 const askPlayer = (scoreOfGame, playerName) => {
   let countRightAnswer = scoreOfGame;
   if (countRightAnswer === 3) {
+    console.log(`Congratulations, ${playerName}!`);
     return;
   }
   const randomNumber = Math.floor(Math.random() * 1000);
@@ -16,6 +17,7 @@ const askPlayer = (scoreOfGame, playerName) => {
   } else {
     console.log(`'${answerUser}' is wrong answer ;(. Correct answer was '${answerCorrect}'\nLet's try again, ${playerName}!`);
     countRightAnswer = 0;
+    return;
   }
   askPlayer(countRightAnswer, playerName);
 };
@@ -24,4 +26,3 @@ console.log('Welcome to the Brain Games!');
 const playerName = readlineSync.question('May I have your name? ');
 console.log(`Hello, ${playerName}!\nAnswer "yes" if the number is even, otherwise answer "no".`);
 askPlayer(0, playerName);
-console.log(`Congratulations, ${playerName}!`);
