02444e3412d5781378e512a32757288b0e4c8277
mede <medegm@mail.ru>
2023-08-30 13:04:42 +0800

    brain-even logic is written

---
+++ bin/brain-even.js
@@ -0,0 +1,28 @@
+#!/usr/bin/env node
+
+import { floor, random } from 'mathjs';
+import readlineSync from 'readline-sync';
+
+const askPlayer = (scoreOfGame, playerName) => {
+  let countRightAnswer = scoreOfGame;
+  if (countRightAnswer === 3) {
+    return;
+  }
+  const randomNumber = floor(random() * 1000);
+  const answerCorrect = randomNumber % 2 === 0 ? 'yes' : 'no';
+  const answerUser = readlineSync.question(`Question: ${randomNumber} Your answer: `);
+  if (answerUser === answerCorrect) {
+    countRightAnswer += 1;
+    console.log('Correct!');
+  } else {
+    console.log(`'${answerUser}' is wrong answer ;(. Correct answer was '${answerCorrect}'\nLet's try again, ${playerName}!`);
+    countRightAnswer = 0;
+  }
+  askPlayer(countRightAnswer, playerName);
+};
+
+console.log('Welcome to the Brain Games!');
+const playerName = readlineSync.question('May I have your name? ');
+console.log(`Hello, ${playerName}!\nAnswer "yes" if the number is even, otherwise answer "no".`);
+askPlayer(0, playerName);
+console.log(`Congratulations, ${playerName}!`);
