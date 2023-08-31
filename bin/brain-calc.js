b163ebabb9f0606efbf649e8fbc9d28e15d92adb
mede <medegm@mail.ru>
2023-08-31 09:45:37 +0800

    step 6. game logic is transferred to index.js

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -1,47 +1,5 @@
 #!/usr/bin/env node
 
-import readlineSync from 'readline-sync';
+import start from '../src/index.js';
 
-const operationsArray = ['+', '-', '*'];
-const maxNumberOfRounds = 3;
-
-const questionBrainCalc = () => {
-  const randomNumber1 = Math.floor(Math.random() * 99);
-  const randomNumber2 = Math.floor(Math.random() * 99);
-  const randomOperation = operationsArray[Math.floor(Math.random() * 2)];
-
-  console.log(`Question: ${randomNumber1} ${randomOperation} ${randomNumber2}`);
-  switch (randomOperation) {
-    case '+':
-      return randomNumber1 + randomNumber2;
-    case '-':
-      return randomNumber1 - randomNumber2;
-    default:
-      return randomNumber1 * randomNumber2;
-  }
-};
-
-const askPlayer = (numberOfRightAnswers, playerName) => {
-  let currentRound = numberOfRightAnswers;
-  if (currentRound === maxNumberOfRounds) {
-    console.log(`Congratulations, ${playerName}!`);
-    return;
-  }
-
-  const answerCorrect = questionBrainCalc();
-  const answerUser = readlineSync.questionInt('Your answer: ');
-  if (answerUser === answerCorrect) {
-    currentRound += 1;
-    console.log('Correct!');
-  } else {
-    console.log(`'${answerUser}' is wrong answer ;(. Correct answer was '${answerCorrect}'\nLet's try again, ${playerName}!`);
-    currentRound = 0;
-    return;
-  }
-  askPlayer(currentRound, playerName);
-};
-
-console.log('Welcome to the Brain Games!');
-const playerName = readlineSync.question('May I have your name? ');
-console.log(`Hello, ${playerName}!\nWhat is the result of the expression?`);
-askPlayer(0, playerName);
+start('brain-calc');
--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,28 +1,5 @@
 #!/usr/bin/env node
 
-import readlineSync from 'readline-sync';
+import start from '../src/index.js';
 
-const askPlayer = (scoreOfGame, playerName) => {
-  let countRightAnswer = scoreOfGame;
-  if (countRightAnswer === 3) {
-    console.log(`Congratulations, ${playerName}!`);
-    return;
-  }
-  const randomNumber = Math.floor(Math.random() * 1000);
-  const answerCorrect = randomNumber % 2 === 0 ? 'yes' : 'no';
-  const answerUser = readlineSync.question(`Question: ${randomNumber} Your answer: `);
-  if (answerUser === answerCorrect) {
-    countRightAnswer += 1;
-    console.log('Correct!');
-  } else {
-    console.log(`'${answerUser}' is wrong answer ;(. Correct answer was '${answerCorrect}'\nLet's try again, ${playerName}!`);
-    countRightAnswer = 0;
-    return;
-  }
-  askPlayer(countRightAnswer, playerName);
-};
-
-console.log('Welcome to the Brain Games!');
-const playerName = readlineSync.question('May I have your name? ');
-console.log(`Hello, ${playerName}!\nAnswer "yes" if the number is even, otherwise answer "no".`);
-askPlayer(0, playerName);
+start('brain-even');
