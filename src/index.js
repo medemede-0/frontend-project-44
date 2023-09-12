afe4b6c5b4502a478512c70788435503d82bdbf6
mede <medegm@mail.ru>
2023-09-12 11:45:44 +0800

    added a recursion to the game result

--- src/index.js
+++ src/index.js
@@ -5,30 +5,41 @@ import {
 
 const maxNumberOfRounds = 3;
 
+const getResultOfGame = (generateRound, numberOfCorrectAnswers) => {
+  const round = generateRound();
+  let result = true;
+
+  if (isPair(round)) {
+    const question = car(round);
+    const answerCorrect = cdr(round);
+    console.log(`Question: ${question}`);
+    const answerUser = readlineSync.question('Your answer: ');
+
+    if (answerUser === answerCorrect.toString()) {
+      console.log('Correct!');
+      if (numberOfCorrectAnswers + 1 === maxNumberOfRounds) {
+        return result;
+      }
+      getResultOfGame(generateRound, numberOfCorrectAnswers + 1);
+    } else {
+      console.log(`'${answerUser}' is wrong answer ;(. Correct answer was '${answerCorrect}'`);
+      result = false;
+    }
+  }
+
+  return result;
+};
+
 export default (generateRound, desc) => {
   console.log('Welcome to the Brain Games!');
 
   const playerName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${playerName}!`);
   console.log(desc);
-  for (let currentRound = 0; currentRound < maxNumberOfRounds; currentRound += 1) {
-    const round = generateRound();
-    if (isPair(round)) {
-      const question = car(round);
-      const answerCorrect = cdr(round);
-      console.log(`Question: ${question}`);
 
-      const answerUser = readlineSync.question('Your answer: ');
-      if (answerUser === answerCorrect.toString()) {
-        console.log('Correct!');
-      } else {
-        console.log(`'${answerUser}' is wrong answer ;(. Correct answer was '${answerCorrect}'\nLet's try again, ${playerName}!`);
-        return;
-      }
-    } else {
-      console.log('Round not generate. Try again');
-      return;
-    }
+  if (getResultOfGame(generateRound, 0)) {
+    console.log(`Congratulations, ${playerName}!`);
+  } else {
+    console.log(`Let's try again, ${playerName}!`);
   }
-  console.log(`Congratulations, ${playerName}!`);
 };
